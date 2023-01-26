3a4a207bb881a29f41615a812b3203c31662fc9e
Harithya Wisesa <43434776+harithya@users.noreply.github.com>
2023-01-26 20:40:48 +0700

    fix posiistion menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -24,27 +24,26 @@ const Header: FC<Props> = ({ hideLogo }) => {
         },
     ];
     return (
-        <header className='container lg:px-16 lg:h-28 h-auto py-2 lg:py-0  flex flex-row   lg:fixed absolute top-0 right-0 left-0 z-20'>
-            <div className='flex lg:justify-between justify-center w-full items-center'>
-                <Link to={'/'}>
-                    <img src='/logo.svg' className={`h-16 lg:${hideLogo ? 'hidden' : 'block'} block`} />
-                </Link>
-                <div>
-
+        <>
+            <header className={`container lg:px-16 lg:h-28 ${!hideLogo && 'bg-white/50 backdrop-blur-md'} h-auto py-2 lg:py-0  flex flex-row   lg:fixed absolute top-0 right-0 left-0 z-20`}>
+                <div className='flex lg:justify-between justify-center w-full items-center'>
+                    <Link to={'/'}>
+                        <img src='/logo.svg' className={`h-16 lg:${hideLogo ? 'hidden' : 'block'} block`} />
+                    </Link>
+                    <ul className='lg:flex hidden space-x-8'>
+                        {links.map((item, key: number) =>
+                            <li key={key.toString()} className="uppercase hover:border-b border-b-primary-500 transition-all duration-500 text-[20px] text-gray-600">
+                                <Link to={item.url}>{item.title}</Link>
+                            </li>)}
+                    </ul>
                 </div>
-                <ul className='lg:flex hidden space-x-8'>
-                    {links.map((item, key: number) =>
-                        <li key={key.toString()} className="uppercase hover:border-b border-b-primary-500 transition-all duration-500 text-[20px] text-gray-600">
-                            <Link to={item.url}>{item.title}</Link>
-                        </li>)}
-                </ul>
-            </div>
-            <button className='lg:hidden fixed right-8 top-8'>
+            </header>
+            <button className='lg:hidden fixed right-8 top-8 z-30'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
                 </svg>
             </button>
-        </header>
+        </>
     )
 }
 
